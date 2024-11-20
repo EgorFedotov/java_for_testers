c856bad1e18960cefaf2cb2efd5543bb7877855c
Egor Fedotov <egorfedotovarz@gmail.com>
2024-11-20 16:59:45 +0300

    изменение конструктора класса на record

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -1,6 +1,7 @@
 package ru.stqa.geometry.figures;
 
-public class Rectangle {
+public record Rectangle(double a, double b) {
+
     public static void printRectangleArea(double a, double b) {
         String text = String.format("Площать прямоугольника со сторонами %f и %f = %f", a,b,rectangleAre(a,b));
         System.out.println(text);
