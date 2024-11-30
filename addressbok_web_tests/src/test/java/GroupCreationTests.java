16fd6e5cb2dd0672c92a8aa4ee624237e3b01a83
Egor Fedotov <egorfedotovarz@gmail.com>
2024-11-30 14:15:22 +0300

    конструктор для группы

--- addressbok_web_tests/src/test/java/GroupCreationTests.java
+++ addressbok_web_tests/src/test/java/GroupCreationTests.java
@@ -1,3 +1,4 @@
+import model.GroupData;
 import org.junit.jupiter.api.Test;
 
 public class GroupCreationTests extends TestBase {
@@ -5,12 +6,21 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void CanCreateGroup() {
         openGroupPage();
-        createGroup("group name", "group header", "group footer");
+        createGroup(new GroupData("group name", "group header", "group footer"));
     }
 
     @Test
     public void CanCreateGroupWithName() {
         openGroupPage();
-        createGroup("", "", "");
+        createGroup(new GroupData());
+    }
+
+
+    @Test
+    public void CanCreateGroupWithNameOnly() {
+        openGroupPage();
+        var emptyGroup = new GroupData();
+        var groupWithName = emptyGroup.withName("some name");
+        createGroup(groupWithName);
     }
 }
