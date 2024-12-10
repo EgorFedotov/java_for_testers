733159e077b9fcac4094a8d578cae6a1cd8ee79f
Egor Fedotov <egorfedotovarz@gmail.com>
2024-12-10 21:20:10 +0300

    Задание №7

--- addressbok_web_tests/src/test/java/manager/ContactHelper.java
+++ addressbok_web_tests/src/test/java/manager/ContactHelper.java
@@ -10,28 +10,39 @@ public class ContactHelper extends HelperBase{
     }
 
     public void CreateContact(ContactData contact) {
-        manager.driver.findElement(By.linkText("add new")).click();
-        manager.driver.findElement(By.name("firstname")).click();
-        manager.driver.findElement(By.name("firstname")).sendKeys(contact.firstname());
-        manager.driver.findElement(By.name("lastname")).click();
-        manager.driver.findElement(By.name("lastname")).sendKeys(contact.lastname());
-        manager.driver.findElement(By.name("address")).click();
-        manager.driver.findElement(By.name("address")).sendKeys(contact.address());
-        manager.driver.findElement(By.name("mobile")).click();
-        manager.driver.findElement(By.name("mobile")).sendKeys(contact.mobile());
-        manager.driver.findElement(By.name("theform")).click();
-        manager.driver.findElement(By.name("email")).click();
-        manager.driver.findElement(By.name("email")).sendKeys(contact.email());
-        manager.driver.findElement(By.cssSelector("input:nth-child(75)")).click();
-        manager.driver.findElement(By.linkText("home page")).click();
+        fillContactForm(contact);
+        returnToHomePAge();
+    }
+
+    public void removeContact() {
+        click(By.name("selected[]"));
+        deleteContact();
+    }
+
+    private void fillContactForm(ContactData contact) {
+        click(By.linkText("add new"));
+        type(By.name("firstname"), contact.firstname());
+        type(By.name("lastname"), contact.lastname());
+        type(By.name("address"), contact.address());
+        type(By.name("mobile"), contact.mobile());
+        click(By.name("theform"));
+        type(By.name("email"), contact.email());
+        submitContactCreation(By.cssSelector("input:nth-child(75)"));
+    }
+
+    private void submitContactCreation(By by) {
+        manager.driver.findElement(by).click();
     }
 
     public boolean isContactPresent() {
       return manager.isElementPresent(By.name("selected[]"));
     }
 
-    public void removeContact() {
-        manager.driver.findElement(By.name("selected[]")).click();
+    private void returnToHomePAge() {
+        manager.driver.findElement(By.linkText("home page")).click();
+    }
+
+    private void deleteContact() {
         manager.driver.findElement(By.cssSelector(".left:nth-child(8) > input")).click();
     }
 }
